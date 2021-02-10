c81c378c78cecdaab70eb004a19438844468deb0
Stephanie Graessle <34826134+sgraessle27@users.noreply.github.com>
2021-02-09 22:40:17 -0600

    Create fizzbuzz.cpp

---
+++ kattis/fizzbuzz.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	int x, y, n;
+	cin >> x >> y >> n;
+
+	for (int i = 1; i <= n; i++) {
+		if (i % x == 0 && i % y == 0)
+			cout << "FizzBuzz\n";
+		else if (i % x == 0)
+			cout << "Fizz\n";
+		else if (i % y == 0)
+			cout << "Buzz\n";
+		else
+			cout << i << endl;
+	}
+
+	//system("pause");
+	return 0;
+}
